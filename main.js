31c181c650f9a40d5c901790ea2b0145a780d880
unknown <minhnghiale1590@gmail.com>
2022-06-14 20:16:24 +0700

    update to collision

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 const canvas=document.getElementById("myCanvas");
-const laneCount=3;
-canvas.width=500;
+const laneCount=7;
+canvas.width=750;
 
 const ctx=canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9,laneCount);
-const car=new Car(road.getMiddleLane(Math.floor(laneCount/2)),0,30,50)
-const sensor=new Sensor(car);
 
+const traffic=[
+    new Car(road.getMiddleLane(2),-100,30,50)
+]
+const car=new Car(road.getMiddleLane(Math.floor(laneCount/2)),0,30,50)
 animate()
 
 function animate(){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.border);
+    }
     car.update(road.border);
     canvas.height=window.innerHeight
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.8);
 
     road.draw(ctx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(ctx);
+    }
     car.draw(ctx);
     
     ctx.restore();
